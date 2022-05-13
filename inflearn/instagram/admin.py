b14d97e267bd600e4ab0f7891b5d53f967fd72c4
hongui <honge7694@naver.com>
2022-05-13 22:52:29 +0900

    Django media 파일 다루기

--- inflearn/instagram/admin.py
+++ inflearn/instagram/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
 
@@ -14,11 +15,17 @@ from .models import Post
 # 방법 3
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'message', 'message_length', 'is_public', 'created_at', 'updated_at'] # list_display : 모델 리스트에 출력할 컬럼을 지정할 수 있다.
+    list_display = ['pk', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at', 'updated_at'] # list_display : 모델 리스트에 출력할 컬럼을 지정할 수 있다.
     list_display_links = ['message'] # list_display_links : 링크가 될 칼럼을 지정할 수 있다.
     list_filter = ['created_at', 'is_public'] # list_filter : 지정 필드값으로 필터링 옵션이 제공된다.
     search_fields = ['message'] # search_fields : admin 내 검색 UI를 통해, DB를 통한 where 쿼리 대상 필드 리스트를 출력한다.
     
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width: 72px;" />')
+        return None
+
     # 모델에 정의되어 있지 않아도 함수로 list_display 가능.
     def message_length(self, post):
         return f'{len(post.message)} 글자'
--- inflearn/instagram/models.py
+++ inflearn/instagram/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True) # auto_now_add : 최초 생성 시간을 1번 사용하여 저장, 갱신 불가능
     updated_at = models.DateTimeField(auto_now=True) # auto_now : 장고 모델이 저장될때마다 현재날짜(시간)으로 갱신, 갱신 가능
@@ -10,7 +11,7 @@ class Post(models.Model):
     def __str__(self):
         # return f"Custom Post object ({self.message})"
         return self.message
-        
+
     # 모델에 정의되어 있지 않아도 함수로 list_display 가능.
     # def message_length(self):
     #     return len(self.message)
