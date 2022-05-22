0c09f3fe509bab9ef47080c5c68d6e39b1e5f538
hongui <honge7694@naver.com>
2022-05-22 17:02:16 +0900

    decorator 사용 방법

--- inflearn/instagram/views.py
+++ inflearn/instagram/views.py
@@ -1,10 +1,38 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView
 
 
-post_list = ListView.as_view(model=Post, paginate_by=10)
+
+
+# CBV decorator 방법1
+# @method_decorator(login_required(), name='dispatch')
+# class PostListView(ListView):
+#     model = Post
+#     paginate_by = 10
+
+
+# post_list = PostListView.as_view()
+
+
+# CBV decorator 방법2
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    paginate_by = 10
+
+post_list = PostListView.as_view()
+
+
+# FBV decorator 방법1
+# post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
+
+
+# FBV decorator 방법2
+# @login_required()
 # def post_list(request):
 #     qs = Post.objects.all()
 #     q = request.GET.get('q', '')
