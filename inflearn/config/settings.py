7494599963bc18524af0aa7ac55a6851f2e7aac8
hongui <honge7694@naver.com>
2022-05-26 17:53:15 +0900

    static 파일 다루기

--- inflearn/config/settings.py
+++ inflearn/config/settings.py
@@ -122,6 +122,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'config', 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
