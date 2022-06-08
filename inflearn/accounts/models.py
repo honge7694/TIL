36aabbab90502299f07f2a4bba897ed8b97f8384
hongui <honge7694@naver.com>
2022-06-08 19:40:49 +0900

    사용자 프로필 페이지 및 프로필 수정

--- inflearn/accounts/models.py
+++ inflearn/accounts/models.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=6)
+    address = models.CharField(max_length=100, blank=True)
+    zipcode = models.CharField(max_length=6, blank=True)
