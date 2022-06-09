2431e2864f0bd1d483eae35130d6ba30184e485b
hongui <honge7694@naver.com>
2022-06-09 15:22:26 +0900

    회원가입

--- inflearn/accounts/views.py
+++ inflearn/accounts/views.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import TemplateView, UpdateView, CreateView
 from .forms import ProfileForm
 from .models import Profile
 
+
+User = get_user_model
 
 # @login_required
 # def profile(request):
@@ -41,9 +45,12 @@ def profile_edit(request):
             'form': form,
         })
 
-
-def signup(request):
-    pass
+signup = CreateView.as_view(
+    model=User,
+    form_class=UserCreationForm,
+    success_url='settings.LOGIN_URL',
+    template_name='accounts/signup_form.html',
+)
 
 
 def logout(request):
