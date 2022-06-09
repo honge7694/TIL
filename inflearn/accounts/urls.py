fe818dc1f9d7ba5bfaefed78bcbfc31a95dc805f
hongui <honge7694@naver.com>
2022-06-09 12:36:02 +0900

    로그인 form 커스텀

--- inflearn/accounts/urls.py
+++ inflearn/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from .forms import LoginForm
 from . import views
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', LoginView.as_view(form_class=LoginForm, template_name='accounts/login_form.html'), name='login'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
 ]
