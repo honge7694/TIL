fc706ee84926308ffade8dd70122598e6e1fba33
hongui <honge7694@naver.com>
2022-06-09 17:40:38 +0900

    로그아웃 구현

--- inflearn/accounts/views.py
+++ inflearn/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -45,13 +45,23 @@ def profile_edit(request):
             'form': form,
         })
 
-signup = CreateView.as_view(
-    model=User,
-    form_class=UserCreationForm,
-    success_url='settings.LOGIN_URL',
-    template_name='accounts/signup_form.html',
-)
+# signup = CreateView.as_view(
+#     model=User,
+#     form_class=UserCreationForm,
+#     success_url='settings.LOGIN_URL',
+#     template_name='accounts/signup_form.html',
+# )
 
 
-def logout(request):
-    pass
+class SignupView(CreateView):
+    model=User
+    form_class=UserCreationForm
+    success_url='settings.LOGIN_REDIRECT_URL'
+    template_name='accounts/signup_form.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        auth_login(self.request, user)
+
+        return response
--- inflearn/config/settings.py
+++ inflearn/config/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -101,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
